08172273421180a13c9378a64b5b0f1ff92d4a7b
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-09-28 20:09:47 -0300

    Create robo3.c

---
+++ robo3.c
@@ -0,0 +1,158 @@
+Peço desculpas pela proporção incorreta dos braços. Vou ajustar as proporções e realocar os braços para criar um robô mais equilibrado. Aqui está uma versão revisada:
+
+```c
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float thetaX = 0.0;
+float thetaY = 0.0;
+
+void cubo() {
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glEnd();
+  glColor3f(0.0, 1.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glEnd();
+  glColor3f(1.0, 1.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
+  glColor3f(1.0, 0.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glEnd();
+  glColor3f(0.0, 1.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
+}
+
+void tronco() {
+  glPushMatrix();
+  glScalef(0.4, 0.4, 0.6);
+  cubo();
+  glPopMatrix();
+}
+
+void bracoDireito() {
+  glPushMatrix();
+  glTranslatef(0.35, 0.0, 0.3);
+  glScalef(0.1, 0.2, 0.4);
+  cubo();
+  glPopMatrix();
+}
+
+void bracoEsquerdo() {
+  glPushMatrix();
+  glTranslatef(-0.35, 0.0, 0.3);
+  glScalef(0.1, 0.2, 0.4);
+  cubo();
+  glPopMatrix();
+}
+
+void pernaDireita() {
+  glPushMatrix();
+  glTranslatef(0.2, 0.0, -0.4);
+  glScalef(0.15, 0.15, 0.4);
+  cubo();
+  glPopMatrix();
+}
+
+void pernaEsquerda() {
+  glPushMatrix();
+  glTranslatef(-0.2, 0.0, -0.4);
+  glScalef(0.15, 0.15, 0.4);
+  cubo();
+  glPopMatrix();
+}
+
+void cabeca() {
+  glPushMatrix();
+  glTranslatef(0.0, 0.0, 0.8);
+  glScalef(0.3, 0.3, 0.3);
+  glutSolidSphere(1.0, 20, 20);
+  glPopMatrix();
+}
+
+void desenha() {
+  glClearColor(0, 0, 0, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  glPushMatrix();
+  glTranslatef(0.0, 0.0, 0.0);
+  glRotatef(thetaX, 1.0, 0.0, 0.0);
+  glRotatef(thetaY, 0.0, 1.0, 0.0);
+  tronco();
+  bracoDireito();
+  bracoEsquerdo();
+  pernaDireita();
+  pernaEsquerda();
+  cabeca();
+  glPopMatrix();
+
+  glFlush();
+}
+
+void tecladoEspecial(int tecla, int x, int y) {
+  switch (tecla) {
+  case GLUT_KEY_RIGHT:
+    thetaY += 2;
+    break;
+  case GLUT_KEY_LEFT:
+    thetaY -= 2;
+    break;
+  case GLUT_KEY_UP:
+    thetaX += 2;
+    break;
+  case GLUT_KEY_DOWN:
+    thetaX -= 2;
+    break;
+  default:
+    break;
+  }
+  glutPostRedisplay();
+}
+
+int main(int argc, char *argv[]) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(400, 400);
+  glutCreateWindow("Robo");
+  glutDisplayFunc(desenha);
+  glutSpecialFunc(tecladoEspecial);
+  glutMainLoop();
+  return 0;
+}
+```
+
+Neste código, ajustei as proporções dos braços para torná-los mais proporcionais e realoquei-os para criar um robô mais equilibrado. Espero que este código atenda às suas expectativas.
