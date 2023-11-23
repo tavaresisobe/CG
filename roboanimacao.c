57e61e1cf1765dbc2881f137f8ab7d932baf4faa
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-11-23 20:27:41 -0300

    Update roboanimacao.c

--- roboanimacao.c
+++ roboanimacao.c
@@ -247,3 +247,123 @@ void init() {
 In this modified `init()` function, I've added material properties for specular reflection and shininess. The `GL_SPECULAR` property controls the specular reflection of light, and `GL_SHININESS` controls the shininess of the material.
 
 These changes should provide more realistic lighting on the arms, legs, and body of the robot. Adjust the material properties according to your preferences for the desired appearance.
+
+
+
+Certainly! Here's an example of how to load a texture using only OpenGL, without relying on external libraries. This example uses a simple BMP (Windows Bitmap) file format. You can replace it with your image file format, but you may need to implement a loader for that specific format.
+
+1. Include the necessary headers in your code:
+
+```c
+#include <GL/glut.h>
+#include <cstdio>
+```
+
+2. Load the texture in the `init()` function:
+
+```c
+GLuint textureID; // Global variable to store the texture ID
+
+void loadTexture() {
+  FILE *file;
+  unsigned char header[54];
+  unsigned int dataPos;
+  unsigned int imageSize;
+  unsigned int width, height;
+  unsigned char *data;
+
+  // Open the file
+  file = fopen("path/to/your/image.bmp", "rb");
+
+  if (!file) {
+    fprintf(stderr, "Could not open file\n");
+    return;
+  }
+
+  // Read the BMP header
+  if (fread(header, 1, 54, file) != 54) {
+    fprintf(stderr, "Not a correct BMP file\n");
+    fclose(file);
+    return;
+  }
+
+  // Check if it's a BMP file
+  if (header[0] != 'B' || header[1] != 'M') {
+    fprintf(stderr, "Not a correct BMP file\n");
+    fclose(file);
+    return;
+  }
+
+  // Read data position and image size from header
+  dataPos = *(int*)&(header[0x0A]);
+  imageSize = *(int*)&(header[0x22]);
+  width = *(int*)&(header[0x12]);
+  height = *(int*)&(header[0x16]);
+
+  // Adjust image size for uncompressed BMP
+  if (imageSize == 0) imageSize = width * height * 3;
+
+  // Set position to the beginning of image data
+  fseek(file, dataPos, SEEK_SET);
+
+  // Allocate buffer for image data
+  data = new unsigned char[imageSize];
+
+  // Read image data
+  fread(data, 1, imageSize, file);
+
+  // Close the file
+  fclose(file);
+
+  // Generate OpenGL texture
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+
+  // Set texture parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Upload image data to OpenGL texture
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+  // Free the allocated buffer
+  delete[] data;
+}
+
+void init() {
+  // ... (existing code)
+
+  // Load texture
+  loadTexture();
+
+  // ... (rest of the init code)
+}
+```
+
+Replace `"path/to/your/image.bmp"` with the actual path to your BMP texture image. Note that this example assumes a 24-bit BMP image. If your image uses a different bit-depth or format, you may need to adjust the code accordingly.
+
+3. Modify your `cubo()` function to include texture coordinates:
+
+```c
+void cubo() {
+  // ... (existing code)
+
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glBegin(GL_POLYGON);
+  glTexCoord2f(0.0, 0.0);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glTexCoord2f(1.0, 0.0);
+  glVertex3f(0.25, 0.25, 0.25);
+  glTexCoord2f(1.0, 1.0);
+  glVertex3f(0.25, -0.25, 0.25);
+  glTexCoord2f(0.0, 1.0);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glEnd();
+
+  // ... (repeat for other sides)
+}
+```
+
+This example uses a simple BMP file format for illustration purposes. Depending on your specific needs, you may need to implement a loader for your particular image file format.
