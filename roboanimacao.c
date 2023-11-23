a6f6aebca8e0df5a0b075ae4a15cf3548701eb67
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-11-23 19:45:04 -0300

    Update roboanimacao.c

--- roboanimacao.c
+++ roboanimacao.c
@@ -3,15 +3,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float thetaX = 0.0;
-float thetaY = 0.0;
-float anguloBracoDireito = 0.0;
-float anguloBracoEsquerdo = 0.0;
-float anguloPernaDireita = 0.0;
-float anguloPernaEsquerda = 0.0;
+float thetaX = 90.00;
+float thetaY = 180.00;
 
 void cubo() {
-  // Função cubo() permanece a mesma
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glEnd();
+
+  glColor3f(0.0, 1.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
+
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glEnd();
+
+  glColor3f(1.0, 1.0, 0.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
+
+  glColor3f(1.0, 0.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, 0.25);
+  glVertex3f(0.25, 0.25, -0.25);
+  glVertex3f(-0.25, 0.25, -0.25);
+  glEnd();
+
+  glColor3f(0.0, 1.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glVertex3f(0.25, -0.25, 0.25);
+  glVertex3f(0.25, -0.25, -0.25);
+  glVertex3f(-0.25, -0.25, -0.25);
+  glEnd();
 }
 
 void tronco() {
@@ -24,7 +66,6 @@ void tronco() {
 void bracoDireito() {
   glPushMatrix();
   glTranslatef(0.15, 0.0, 0.0);
-  glRotatef(anguloBracoDireito, 1.0, 0.0, 0.0); // Rotação no braço direito
   glScalef(0.15, 0.15, 0.4);
   cubo();
   glPopMatrix();
@@ -33,7 +74,6 @@ void bracoDireito() {
 void bracoEsquerdo() {
   glPushMatrix();
   glTranslatef(-0.15, 0.0, 0.0);
-  glRotatef(anguloBracoEsquerdo, 1.0, 0.0, 0.0); // Rotação no braço esquerdo
   glScalef(0.15, 0.15, 0.4);
   cubo();
   glPopMatrix();
@@ -42,7 +82,6 @@ void bracoEsquerdo() {
 void pernaDireita() {
   glPushMatrix();
   glTranslatef(0.06, 0.0, -0.25);
-  glRotatef(anguloPernaDireita, 1.0, 0.0, 0.0); // Rotação na perna direita
   glScalef(0.15, 0.15, 0.4);
   cubo();
   glPopMatrix();
@@ -51,7 +90,6 @@ void pernaDireita() {
 void pernaEsquerda() {
   glPushMatrix();
   glTranslatef(-0.06, 0.0, -0.25);
-  glRotatef(anguloPernaEsquerda, 1.0, 0.0, 0.0); // Rotação na perna esquerda
   glScalef(0.15, 0.15, 0.4);
   cubo();
   glPopMatrix();
@@ -61,7 +99,7 @@ void cabeca() {
   glPushMatrix();
   glTranslatef(0.0, 0.0, 0.2);
   glScalef(0.25, 0.25, 0.25);
-  cubo();
+  cubo() ;
   glPopMatrix();
 }
 
@@ -71,11 +109,12 @@ void desenha() {
   glEnable(GL_DEPTH_TEST);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-
   glPushMatrix();
+
   glTranslatef(0.0, 0.0, 0.0);
   glRotatef(thetaX, 1.0, 0.0, 0.0);
   glRotatef(thetaY, 0.0, 1.0, 0.0);
+
   tronco();
   bracoDireito();
   bracoEsquerdo();
@@ -83,7 +122,6 @@ void desenha() {
   pernaEsquerda();
   cabeca();
   glPopMatrix();
-
   glFlush();
 }
 
@@ -100,30 +138,6 @@ void tecladoEspecial(int tecla, int x, int y) {
     break;
   case GLUT_KEY_DOWN:
     thetaX -= 2;
-    break;
-  case GLUT_KEY_PAGE_UP:
-    anguloBracoDireito += 2; // Aumenta o ângulo do braço direito
-    break;
-  case GLUT_KEY_PAGE_DOWN:
-    anguloBracoDireito -= 2; // Diminui o ângulo do braço direito
-    break;
-  case GLUT_KEY_HOME:
-    anguloBracoEsquerdo += 2; // Aumenta o ângulo do braço esquerdo
-    break;
-  case GLUT_KEY_END:
-    anguloBracoEsquerdo -= 2; // Diminui o ângulo do braço esquerdo
-    break;
-  case GLUT_KEY_INSERT:
-    anguloPernaDireita += 2; // Aumenta o ângulo da perna direita
-    break;
-  case GLUT_KEY_DELETE:
-    anguloPernaDireita -= 2; // Diminui o ângulo da perna direita
-    break;
-  case GLUT_KEY_F1:
-    anguloPernaEsquerda += 2; // Aumenta o ângulo da perna esquerda
-    break;
-  case GLUT_KEY_F2:
-    anguloPernaEsquerda -= 2; // Diminui o ângulo da perna esquerda
     break;
   default:
     break;
