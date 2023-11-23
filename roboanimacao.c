272e623ca3ac7e7509edae64f750315778790efe
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-11-23 20:39:58 -0300

    Update roboanimacao.c

--- roboanimacao.c
+++ roboanimacao.c
@@ -367,3 +367,192 @@ void cubo() {
 ```
 
 This example uses a simple BMP file format for illustration purposes. Depending on your specific needs, you may need to implement a loader for your particular image file format.
+
+  Certainly! Below is the complete modified code with different textures for each part of the robot:
+
+```c
+#include <GL/glut.h>
+#include <cstdio>
+
+// Struct to hold texture information
+struct Texture {
+  GLuint id;
+  int width;
+  int height;
+  unsigned char* data;
+};
+
+// Global variables for robot angles
+float angleBracoDireito = 0.0;
+float angleBracoEsquerdo = 0.0;
+float anglePernaDireita = 0.0;
+float anglePernaEsquerda = 0.0;
+
+float thetaX = 90.00;
+float thetaY = 180.00;
+
+// Global variables for robot textures
+Texture robotTextures[6]; // One for each surface
+Texture earthTexture;
+
+// Function to load texture data
+void loadTextureData(const char* filename, Texture& texture) {
+  FILE* file = fopen(filename, "rb");
+
+  if (!file) {
+    fprintf(stderr, "Could not open file\n");
+    return;
+  }
+
+  // Assuming it's a simple 24-bit BMP file
+  fseek(file, 18, SEEK_SET);
+  fread(&texture.width, sizeof(int), 1, file);
+  fread(&texture.height, sizeof(int), 1, file);
+  int imageSize = texture.width * texture.height * 3;
+
+  texture.data = new unsigned char[imageSize];
+  fread(texture.data, sizeof(unsigned char), imageSize, file);
+
+  fclose(file);
+}
+
+// Function to initialize texture
+void initTexture(Texture& texture) {
+  glGenTextures(1, &texture.id);
+  glBindTexture(GL_TEXTURE_2D, texture.id);
+
+  // Set texture parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Upload image data to OpenGL texture
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture.width, texture.height, 0, GL_BGR, GL_UNSIGNED_BYTE, texture.data);
+
+  // Free the allocated buffer
+  delete[] texture.data;
+}
+
+// Function to initialize all textures
+void initTextures() {
+  loadTextureData("path/to/your/surface1.bmp", robotTextures[0]);
+  initTexture(robotTextures[0]);
+
+  loadTextureData("path/to/your/surface2.bmp", robotTextures[1]);
+  initTexture(robotTextures[1]);
+
+  loadTextureData("path/to/your/surface3.bmp", robotTextures[2]);
+  initTexture(robotTextures[2]);
+
+  loadTextureData("path/to/your/surface4.bmp", robotTextures[3]);
+  initTexture(robotTextures[3]);
+
+  loadTextureData("path/to/your/surface5.bmp", robotTextures[4]);
+  initTexture(robotTextures[4]);
+
+  loadTextureData("path/to/your/surface6.bmp", robotTextures[5]);
+  initTexture(robotTextures[5]);
+
+  loadTextureData("path/to/your/earthTexture.bmp", earthTexture);
+  initTexture(earthTexture);
+}
+
+// Function to draw a textured cube
+void cubo(int textureIndex) {
+  glBindTexture(GL_TEXTURE_2D, robotTextures[textureIndex].id);
+
+  glBegin(GL_POLYGON);
+  glTexCoord2f(0.0, 0.0);
+  glVertex3f(-0.25, 0.25, 0.25);
+
+  glTexCoord2f(1.0, 0.0);
+  glVertex3f(0.25, 0.25, 0.25);
+
+  glTexCoord2f(1.0, 1.0);
+  glVertex3f(0.25, -0.25, 0.25);
+
+  glTexCoord2f(0.0, 1.0);
+  glVertex3f(-0.25, -0.25, 0.25);
+  glEnd();
+
+  // ... (repeat for other sides)
+}
+
+// Update your existing draw functions to use the modified cubo function
+void tronco() {
+  glPushMatrix();
+  glScalef(0.4, 0.4, 0.6);
+  cubo(0); // Use texture for surface1
+  glPopMatrix();
+}
+
+void bracoDireito() {
+  glPushMatrix();
+  glTranslatef(0.15, 0.0, 0.0);
+  glRotatef(angleBracoDireito, 1.0, 0.0, 0.0);
+  glScalef(0.15, 0.15, 0.4);
+  cubo(1); // Use texture for surface2
+  glPopMatrix();
+}
+
+void bracoEsquerdo() {
+  glPushMatrix();
+  glTranslatef(-0.15, 0.0, 0.0);
+  glRotatef(angleBracoEsquerdo, 1.0, 0.0, 0.0);
+  glScalef(0.15, 0.15, 0.4);
+  cubo(2); // Use texture for surface3
+  glPopMatrix();
+}
+
+void pernaDireita() {
+  glPushMatrix();
+  glTranslatef(0.06, 0.0, -0.25);
+  glRotatef(anglePernaDireita, 1.0, 0.0, 0.0);
+  glScalef(0.15, 0.15, 0.4);
+  cubo(3); // Use texture for surface4
+  glPopMatrix();
+}
+
+void pernaEsquerda() {
+  glPushMatrix();
+  glTranslatef(-0.06, 0.0, -0.25);
+  glRotatef(anglePernaEsquerda, 1.0, 0.0, 0.0);
+  glScalef(0.15, 0.15, 0.4);
+  cubo(4); // Use texture for surface5
+  glPopMatrix();
+}
+
+void cabeca() {
+  glPushMatrix();
+  glTranslatef(0.0, 0.0, 0.2);
+  glScalef(0.25, 0.25, 0.25);
+  cubo(5); // Use texture for surface6
+  glPopMatrix();
+}
+
+void desenha() {
+  glClearColor(0, 0, 0, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glPushMatrix();
+
+  glTranslatef(0.0, 0.0, 0.0);
+  glRotatef(thetaX, 1.0, 0.0, 0.0);
+  glRotatef(thetaY, 0.0, 1.0, 0.0);
+
+  tronco();
+  bracoDireito();
+  bracoEsquerdo();
+  pernaDireita();
+  pernaEsquerda();
+  cabeca();
+
+  glPopMatrix();
+  glFlush();
+}
+
+void tecladoEspecial(int tecla, int x, int y) {
+  switch (tecla)
